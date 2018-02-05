17c7e9fc245b3aa807bff1831408f96c5cf18eb5
Louis Giraud <louis.giraud@epitech.eu>
2018-02-05 13:03:02 +0100

    [Project Update] adding keys for Flickr api

--- app/src/main/java/eu/epitech/epicture/api/flickr/Client.java
+++ app/src/main/java/eu/epitech/epicture/api/flickr/Client.java
@@ -6,7 +6,7 @@ package eu.epitech.epicture.api.flickr;
 
 public class Client {
 
-    public static String _ID = "";
-    public static String _SECRET = "";
+    public static String _ID = "4967b2b24eb6cb809a6f80d47ec3cb80";
+    public static String _SECRET = "4799fefc7b6bcc62";
 
 }
