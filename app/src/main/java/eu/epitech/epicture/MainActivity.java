e639d84c2c35c46eba35854c80fad12c3c083b17
Louis Giraud <louis.giraud@epitech.eu>
2018-02-09 15:16:02 +0100

    Update layout adding search tab in the MainLayout adding item tab in the MainLayout adding some dynamics content adding the begin of the gallery

--- app/src/main/java/eu/epitech/epicture/MainActivity.java
+++ app/src/main/java/eu/epitech/epicture/MainActivity.java
@@ -1,52 +1,76 @@
 package eu.epitech.epicture;
 
-import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    // déclaré interface imgur en private
+    // déclaré interfacxe flickr en private
+    // déclaré interface pixabay en private
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void onSearchSuccess(ArrayList<String> images) {
+        for (String imgAddrs : images)
+        {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    public void onSearchError() {
+    }
+
+    public void searchDisplay(int visibility) {
+    }
+
+    public void onFavoriteButtonClick(View view) {
+
+    }
+
+    public void onUploadButtonClick(View view) {
+
+    }
+
+    public void onSettingClick(View view) {
+
+    }
+
+    public void onImgurSearch(View view) {
+        onSearchClick(view);
+    }
+
+
+    public void onFlickrSearch(View view) {
+        onSearchClick(view);
+    }
+
+
+    public void onPixabaySearch(View view) {
+        onSearchClick(view);
+    }
+
+    public void onSearchClick(View view) {
+        FloatingActionButton button = findViewById(R.id.floating_search_button);
+        RelativeLayout searchLayout = findViewById(R.id.search_layout);
+        if (searchLayout.getVisibility() == View.INVISIBLE) {
+            searchLayout.setVisibility(View.VISIBLE);
+            button.setImageResource(R.drawable.ic_close);
+        } else {
+            searchLayout.setVisibility(View.INVISIBLE);
+            button.setImageResource(R.drawable.ic_search);
+        }
     }
 }
